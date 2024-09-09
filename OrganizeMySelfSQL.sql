0b0511be7650be8d5b039c4a4ab3cf99dc0b9b4a
Pasquale <61435712+Psq333@users.noreply.github.com>
2024-09-09 22:08:18 +0200

    Update OrganizeMySelfSQL.sql

--- OrganizeMySelfSQL.sql
+++ OrganizeMySelfSQL.sql
@@ -1,16 +1,15 @@
-CREATE DATABASE OrganizeMySelf;
-
 USE OrganizeMySelf
 CREATE TABLE Type(
-	id int not null primary key Identity(1,1),
+	id int not null primary key auto_increment,
 	type varchar(25) not null
 );
 USE OrganizeMySelf
 CREATE TABLE Storage(
-	id int not null primary key Identity(1,1),
+	id int not null primary key auto_increment,
 	date date not null,
 	id_type int not null,
 	inside int not null,
-	cash decimal not null,
+	cash float not null,
+	descrizione varchar(255),
 	constraint TypeConstraint FOREIGN KEY (id_type) references Type(id)
 );
